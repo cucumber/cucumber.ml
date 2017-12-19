27ca2c815781db29ac83b3a5777368f88af40a98
Chris Yocum <cyocum@gmail.com>
2017-12-19 16:50:15 +0000

    Spilt out step as a seperate module

---
+++ src/step.ml
@@ -0,0 +1,11 @@
+type arg = String | Table
+
+type t = {
+    locations : Location.t list;
+    text : string;
+    argument : arg option
+  }    
+
+let string_of_step step =  
+  let loc_str = List.fold_left (fun accum loc -> accum ^ (Location.string_of_location loc)) "" step.locations in
+  "step text: " ^ step.text ^ "\n" ^ loc_str
