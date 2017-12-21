ad9ce1bd57c202153906b28fbd9aaf04f40d4119
Chris Yocum <cyocum@gmail.com>
2017-12-21 14:34:25 +0000

    Added step arguments
    * Added step arguments but left in a dummy int for the Datatable one
      so I can figure out what to do with it once I have the dynlink setup

--- lib/cucumber.ml
+++ lib/cucumber.ml
@@ -1,13 +1,14 @@
 type outcome = Pass | Fail | Pending
-
+type args = Datatable of int | Docstring of string | None
+                           
 let string_of_outcome = function
-  | Pass -> "P"
+  | Pass -> "."
   | Fail -> "F"
-  | Pending -> "Pend"    
+  | Pending -> "P"
                            
 type step = {
     regex : Re.re;
-    step : (Re.groups option -> outcome)
+    step : (Re.groups option -> args -> outcome)
   }
 
 type t = step list
@@ -22,7 +23,7 @@ let find str {regex; step} =
 
 let actuate step str =
   let groups = (Re.exec_opt step.regex str) in
-  step.step groups
+  step.step groups None
   
 let run cucc str =
   match (List.find_opt (find str) cucc) with
--- lib/cucumber.mli
+++ lib/cucumber.mli
@@ -1,7 +1,9 @@
 type outcome = Pass | Fail | Pending
+
+type args = Datatable of int | Docstring of string | None
 type t
 
 val empty : t
-val given : t -> Re.re -> (Re.groups option -> outcome) -> t
+val given : t -> Re.re -> (Re.groups option -> args -> outcome) -> t
 val run : t -> string -> outcome
 val string_of_outcome : outcome -> string
