e13f9047e7a525271dddc4ebb1e77d14a190cd90
Peter Garamvolgyi <peter.garamvolgyi@hotmail.com>
2018-03-15 12:03:25 +0100

    Fix test execution order

--- src/olympic.ml
+++ src/olympic.ml
@@ -7,18 +7,21 @@ let load_test_plugin fname =
      print_endline ("ERROR loading plugin: " ^ (Dynlink.error_message err));
      raise e
 
+let load_feature_file fname =
+  let pickleLst = Gherkin.load_feature_file fname in
+  List.rev_map (fun p -> {p with Cucumber.Pickle.steps = (List.rev p.Cucumber.Pickle.steps)}) pickleLst
+
 let unpack_pickle pickle =
   pickle.Cucumber.Pickle.steps
      
 let _ =
   load_test_plugin Sys.argv.(1);
-  let pickleLst = Gherkin.load_feature_file Sys.argv.(2) in
+  let pickleLst = load_feature_file Sys.argv.(2) in
   match pickleLst with
   | [] -> print_endline "Empty Pickle list"
   | _ ->
      let module C = (val Cucumber.Lib.get_tests () : Cucumber.Lib.TEST_PLUGIN) in
-     let stepLst = List.flatten (List.rev_map (unpack_pickle) pickleLst) in
-     let outcomeLst = List.rev_map (Cucumber.Lib.run (C.get_tests ())) stepLst in
+     let stepLst = List.flatten (List.map (unpack_pickle) pickleLst) in
+     let outcomeLst = List.map (Cucumber.Lib.run (C.get_tests ())) stepLst in
      List.iter (fun o -> (print_string (Cucumber.Lib.string_of_outcome o))) outcomeLst;
      print_newline ()
-
