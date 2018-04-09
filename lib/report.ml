1ecf26d243a9f8d79575dfcf1730935225bd6364
Chris Yocum <cy185017@ncr.com>
2018-04-09 10:29:31 +0100

    Updated based on reviewer feedback.

--- lib/report.ml
+++ lib/report.ml
@@ -15,7 +15,7 @@ let print_scenario_report outcomeLists =
   in
 
   let passed = scenarios - failed - undefined in
-
+  
   let stats = [] in
     let stats =
       if passed > 0
@@ -35,40 +35,26 @@ let print_scenario_report outcomeLists =
 
 let print_step_report outcomes =
   let steps = List.length outcomes in
-  let failed = Outcome.count_failed outcomes in
-  let undefined = Outcome.count_undefined outcomes in
-  let skipped = Outcome.count_skipped outcomes in
-  let pending = Outcome.count_pending outcomes in
-  let passed = Outcome.count_passed outcomes in
-  
-  let stats = [] in
-    let stats =
-      if passed > 0
-      then (string_of_int passed ^ " passed") :: stats
-      else stats in
-    let stats =
-      if pending > 0
-      then (string_of_int pending ^ " pending") :: stats
-      else stats in
-    let stats =
-      if skipped > 0
-      then (string_of_int skipped ^ " skipped") :: stats
-      else stats in
-    let stats =
-      if undefined > 0
-      then (string_of_int undefined ^ " undefined") :: stats
-      else stats in
-    let stats =
-      if failed > 0
-      then (string_of_int failed ^ " failed") :: stats
-      else stats in
-
+  let stats = [
+          ("passed",    Outcome.count_passed outcomes);
+          ("pending",   Outcome.count_pending outcomes);
+          ("skipped",   Outcome.count_skipped outcomes);
+          ("undefined", Outcome.count_undefined outcomes);
+          ("failed",    Outcome.count_failed outcomes);
+        ]
+  in
+  let formattedStats = Base.List.fold stats ~init:[] ~f:(fun acc (text, count) ->
+                  if count > 0
+                  then (string_of_int count ^ " " ^ text) :: acc
+                  else acc
+                )
+  in 
   print_string (string_of_int steps ^ " steps ");
-  print_string "("; print_list stats; print_endline ")"
+  print_string "("; print_list formattedStats; print_endline ")"
 
 let print outcomeLists =
   let outcomes = List.flatten outcomeLists in
   print_scenario_report outcomeLists;
   print_step_report outcomes;
-  List.iter (fun o -> (print_string (Outcome.string_of_outcome o))) outcomes;
+  Base.List.iter  outcomes (fun o -> (print_string (Outcome.string_of_outcome o)));
   print_newline ()
