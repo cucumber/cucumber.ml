2f8b55962d261f40c4f1cb21575eef5e70640903
Chris Yocum <cyocum@gmail.com>
2018-05-03 21:34:47 +0100

    Fixed a couple of function signatures

--- lib/table.mli
+++ lib/table.mli
@@ -1,5 +1,5 @@
 type t
 
 val string_of_table : t -> string
-val to_map_with_header : t -> (Base.String.t, string list, Base.String.comparator_witness) Base.Map.t
-val transform : t -> (string Base.List.t -> 'a) -> 'a Base.List.t
+val to_map_with_header : t -> (Base.String.t, Base.String.t list, Base.String.comparator_witness) Base.Map.t
+val transform : t -> (Base.String.t Base.List.t -> 'a) -> 'a Base.List.t
