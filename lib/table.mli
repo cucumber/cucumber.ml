efb1933e37fa3d4e7c0676f4c645b057a4a5ebef
Chris Yocum <cyocum@gmail.com>
2018-05-04 15:17:35 +0100

    Updated table.mli to be consistent with respect to Base.String.t

--- lib/table.mli
+++ lib/table.mli
@@ -3,5 +3,5 @@ type t
 val string_of_table : t -> string
 val to_map_with_header : t -> (Base.String.t, Base.String.t list, Base.String.comparator_witness) Base.Map.t
 val transform : t -> (Base.String.t Base.List.t -> 'a) -> 'a Base.List.t
-val transform_with_header : t -> (string Base.List.t -> string Base.List.t -> 'a) -> 'a Base.List.t
-val to_map_with_col_header : t -> (Base.String.t, string list, Base.String.comparator_witness) Base.Map.t
+val transform_with_header : t -> (Base.String.t Base.List.t -> Base.String.t Base.List.t -> 'a) -> 'a Base.List.t
+val to_map_with_col_header : t -> (Base.String.t, Base.String.t list, Base.String.comparator_witness) Base.Map.t
