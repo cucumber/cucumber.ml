592961abe7fde4ff04ce77882fcc68e2bf7923bc
Christopher Yocum <cyocum@gmail.com>
2020-07-31 15:53:51 +0100

    Cleaned up the example test code to be a bit more readable

--- test/test_parallel.ml
+++ test/test_parallel.ml
@@ -9,67 +9,76 @@ let bar state outcome =
      (Some { foo = x.foo + 1 }, Cucumber.OutcomeManager.add outcome Cucumber.Outcome.Pass)
   | None ->
      (Some { foo = 12}, Cucumber.OutcomeManager.add outcome Cucumber.Outcome.Pass)
-          
+
+let first_when_func group arg (foo_state_opt, outcome) =
+  Lwt_io.printl "I am in the when: I have a thing to do" >>= (fun _ ->
+    let bar =
+      match foo_state_opt with
+      | Some x ->
+         (Some { foo = (succ x.foo)}, (Cucumber.OutcomeManager.add outcome Cucumber.Outcome.Pass))
+      | None ->
+         (Some { foo = 1}, (Cucumber.OutcomeManager.add outcome Cucumber.Outcome.Pass))
+    in
+    Lwt.return bar)  
+
+let first_given_func group arg (foo_state_opt, outcome) =
+  Lwt_io.printl "I am in the given: I have a simple background" >>= (fun _ -> Lwt.return (bar foo_state_opt outcome)) 
+
+let first_then_func group arg (foo_state_opt, outcome) =
+  match foo_state_opt with
+  | Some x ->
+     (Lwt_io.printl "I am in the then: I have done the thing" ) >>= (fun _ -> Lwt.return (Some {foo = x.foo + 1}, (Cucumber.OutcomeManager.add outcome Cucumber.Outcome.Pass)))
+  | None ->
+     Lwt.return (Some { foo = 1}, (Cucumber.OutcomeManager.add outcome Cucumber.Outcome.Pass))
+
+let second_given_func group arg (foo_state_opt, outcome) =
+  Lwt_io.printl "I am in the other given" >>= (fun _ -> Lwt.return (bar foo_state_opt outcome))            
+
+let second_when_func group arg (foo_state_opt, outcome) =
+  Lwt_io.printl "I am in the other when" >>= (fun _ ->
+    let bar =
+      match foo_state_opt with
+      | Some x ->
+         (Some { foo = (succ x.foo)}, Cucumber.OutcomeManager.add outcome Cucumber.Outcome.Pass)
+      | None ->
+         (Some { foo = 1}, Cucumber.OutcomeManager.add outcome Cucumber.Outcome.Pass)
+    in
+    Lwt.return bar)
+
+let second_then_func group arg (foo_state_opt, outcome) =
+  match foo_state_opt with
+  | Some x ->
+     Lwt.return (Some {foo = x.foo + 1}, Cucumber.OutcomeManager.add outcome Cucumber.Outcome.Pass)
+  | None ->
+     Lwt.return (Some { foo = 1}, Cucumber.OutcomeManager.add outcome Cucumber.Outcome.Pass)
+  
+  
 (* users can use the pipeline operator *)
 let foo = Cucumber.LibParallel.empty
           |>
             (Cucumber.LibParallel._When
                (Re.Perl.compile_pat "I have a thing to do")
-               (fun group arg (foo_state_opt, outcome) ->
-                 Lwt_io.printl "I am in the when: I have a thing to do" >>= (fun _ ->
-                  let bar =
-                    match foo_state_opt with
-                    | Some x ->
-                       (Some { foo = (succ x.foo)}, (Cucumber.OutcomeManager.add outcome Cucumber.Outcome.Pass))
-                    | None ->
-                       (Some { foo = 1}, (Cucumber.OutcomeManager.add outcome Cucumber.Outcome.Pass))
-                  in
-                  Lwt.return bar)))
+               first_when_func)
           |> 
             (Cucumber.LibParallel._Given
-              (Re.Perl.compile_pat "I have a simple background")
-              (fun group arg (foo_state_opt, outcome) ->
-                Lwt_io.printl "I am in the given: I have a simple background" >>= (fun _ -> Lwt.return (bar foo_state_opt outcome)) 
-              ))
+               (Re.Perl.compile_pat "I have a simple background")
+               first_given_func)
           |>
             (Cucumber.LibParallel._Then
                (Re.Perl.compile_pat "I have done the thing")
-               (fun group arg (foo_state_opt, outcome) ->
-                        match foo_state_opt with
-                        | Some x ->
-                           (Lwt_io.printl "I am in the then: I have done the thing" ) >>= (fun _ -> Lwt.return (Some {foo = x.foo + 1}, (Cucumber.OutcomeManager.add outcome Cucumber.Outcome.Pass)))
-                        | None ->
-                           Lwt.return (Some { foo = 1}, (Cucumber.OutcomeManager.add outcome Cucumber.Outcome.Pass))                          
-            ))
+               first_then_func)
           |>
             (Cucumber.LibParallel._Given
               (Re.Perl.compile_pat "I have another simple background")
-              (fun group arg (foo_state_opt, outcome) ->
-                Lwt_io.printl "I am in the other given" >>= (fun _ -> Lwt.return (bar foo_state_opt outcome)) 
-            ))
+              second_given_func)
           |>
             (Cucumber.LibParallel._When
                (Re.Perl.compile_pat "I have some other thing to do")
-               (fun group arg (foo_state_opt, outcome) ->
-                 Lwt_io.printl "I am in the other when" >>= (fun _ ->
-                  let bar =
-                    match foo_state_opt with
-                    | Some x ->
-                       (Some { foo = (succ x.foo)}, Cucumber.OutcomeManager.add outcome Cucumber.Outcome.Pass)
-                    | None ->
-                       (Some { foo = 1}, Cucumber.OutcomeManager.add outcome Cucumber.Outcome.Pass)
-                  in
-                  Lwt.return bar)))
+               second_when_func)
           |>
             (Cucumber.LibParallel._Then
                (Re.Perl.compile_pat "I should have done the thing")
-               (fun group arg (foo_state_opt, outcome) ->
-                 match foo_state_opt with
-                 | Some x ->
-                    Lwt.return (Some {foo = x.foo + 1}, Cucumber.OutcomeManager.add outcome Cucumber.Outcome.Pass)
-                 | None ->
-                    Lwt.return (Some { foo = 1}, Cucumber.OutcomeManager.add outcome Cucumber.Outcome.Pass)
-            ))
+               second_then_func)
           
 let _ =
   Base.List.iter (Cucumber.LibParallel.execute foo "test/test_parallel.feature" None)
